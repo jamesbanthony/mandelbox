febb1f928501f11fbbada76f19187510503354b6
mceglow <marekc@live.com>
2015-04-02 17:19:19 -0400

    Updated run.sh

--- run.sh
+++ run.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
-START=$1
-END=$2
+START=1
+if [ $# -eq 0 ]
+  then
+    echo "No arguments supplied, using 120 frames..."
+    END=120
+  else
+  	END=$1
+fi
 
-python create.py $1 $2
+echo -n "Creating .dat files..."
+python create.py $END
+echo " .dat files created."
 for i in `seq $START $END`;
 do
-	echo working on $i;
-    ./mandelbox ./params$i.dat
+	echo -en "\e[1K\rGenerating mandelbox image $i/$END...";
+    ./mandelbox ./params$i.dat;
+    mogrify -format jpg image$i.bmp;
+    rm image$i.bmp
 done
+echo " Finished rendering images."
+echo -n "Removing .dat files..."
 rm params*.dat
-ffmpeg -f image2 -r 30 -i image%d.bmp ./out.mov
+echo " .dat files removed."
+echo "Generating video..."
+ffmpeg -start_number 1 -i image%d.jpg -c:v libx264 -r 30 -pix_fmt yuv420p out.mp4
+echo -n "Done!"
+vlc out.avi
